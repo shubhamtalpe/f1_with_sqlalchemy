247ef369e2d17940a43ff63528bc3deb3dddf3a8
shubhamtalpe <shubhamtalpeofficial@gmail.com>
2024-04-24 22:36:32 +0530

    logging added to main

--- data_readers/lap_times_reader.py
+++ data_readers/lap_times_reader.py
@@ -5,6 +5,9 @@ from classes_module.lap_times import LapTimes
 from supporing_scripts.custom_decorators import get_time
 import multiprocessing as mp
 import numpy as np
+from functools import reduce
+from operator import add
+from sqlalchemy.orm import Session
 
 class LapTimesReader:
     
@@ -16,13 +19,13 @@ class LapTimesReader:
         if not os.path.isfile(self.file_path):
             raise FileNotFoundError(f'Required file not found at path {self.file_path}')
 
-    def create_lap_times_obj(self, data : np.ndarray) -> LapTimes :
-        return LapTimes(RaceId=data[0],
+    def create_lap_times_obj(self, chunk : np.ndarray) -> List[LapTimes] :
+        return [LapTimes(RaceId=data[0],
                         DriverId=data[1],
                         LapNumber=data[2],
                         Position=data[3],
                         Time=data[4],
-                        TimeInMiliseconds=data[5])
+                        TimeInMiliseconds=data[5]) for data in chunk]
     
     def readFile(self) -> List[LapTimes] :
         self.data = pd.read_csv(self.file_path)
@@ -30,6 +33,6 @@ class LapTimesReader:
         num_processes : int = mp.cpu_count()
         chunk_size : int = data_values.shape[0] // num_processes
         chunks : List[np.ndarray] = [data_values[idx : idx + chunk_size : ] for idx in range(0, data_values.shape[0], chunk_size)]
-        pool : mp.Pool = mp.Pool(processes=num_processes)
-        LapTimesData : List[LapTimes] = pool.map(self.create_lap_times_obj, chunks)
+        with mp.Pool(processes=num_processes) as pool:
+            LapTimesData : List[LapTimes] = reduce(add, pool.map(self.create_lap_times_obj, chunks))
         return LapTimesData
--- main_module/main.py
+++ main_module/main.py
@@ -32,85 +32,133 @@ from data_readers.lap_times_reader import LapTimesReader
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from supporing_scripts.custom_decorators import get_time
+from concurrent.futures import ThreadPoolExecutor
+from tqdm import tqdm
+import logging
 
 @get_time
 def read_data_from_files() -> None :
+    logging.info("Reading files and loading data to database")
+    
+    logging.debug("Creating engine object")
     engine = create_engine("sqlite:///:memory:")
     # engine = create_engine("sqlite:///sample.db")
+
+    logging.debug("Creating session object")
     Session = sessionmaker(bind=engine)
-
     Base.metadata.create_all(engine, checkfirst=True)
-    session = Session()
+    
+    with Session() as session:
+        logging.debug("Session created")
 
-    obj = CircuitReader()
-    session.add_all(obj.readFile())
-    session.commit()
-    del obj
+        logging.debug("Starting to read files")
+        with tqdm(total=14) as pbar:
+            obj = CircuitReader()
+            session.add_all(obj.readFile())
+            session.commit()
+            del obj
+            pbar.update(1)
+            logging.debug("Circuits data loaded")
 
-    obj = SeasonsReader()
-    session.add_all(obj.readFile())
-    session.commit()
-    del obj
+            obj = SeasonsReader()
+            session.add_all(obj.readFile())
+            session.commit()
+            del obj
+            pbar.update(1)
+            logging.debug("Seasons data loaded")
 
-    obj = StatusReader()
-    session.add_all(obj.readFile())
-    session.commit()
-    del obj
+            obj = StatusReader()
+            session.add_all(obj.readFile())
+            session.commit()
+            del obj
+            pbar.update(1)
+            logging.debug("Status data loaded")
 
-    obj = ConstructorsReader()
-    session.add_all(obj.readFile())
-    session.commit()
-    del obj
+            obj = ConstructorsReader()
+            session.add_all(obj.readFile())
+            session.commit()
+            del obj
+            pbar.update(1)
+            logging.debug("Constructor data loaded")
 
-    obj = DriversReader()
-    session.add_all(obj.readFile())
-    session.commit()
-    del obj
+            obj = DriversReader()
+            session.add_all(obj.readFile())
+            session.commit()
+            del obj
+            pbar.update(1)
+            logging.debug("Driver data loaded")
 
-    obj = RacesReader()
-    session.add_all(obj.readFile())
-    session.commit()
-    del obj
+            obj = RacesReader()
+            session.add_all(obj.readFile())
+            session.commit()
+            del obj
+            pbar.update(1)
+            logging.debug("Races data loaded")
 
-    obj = ConstructorStandingsReader()
-    session.add_all(obj.readFile())
-    session.commit()
-    del obj
+            obj = ConstructorStandingsReader()
+            session.add_all(obj.readFile())
+            session.commit()
+            del obj
+            pbar.update(1)
+            logging.debug("Constructor Standings data loaded")
 
-    obj = ConstructorResultsReader()
-    session.add_all(obj.readFile())
-    session.commit()
-    del obj
+            obj = ConstructorResultsReader()
+            session.add_all(obj.readFile())
+            session.commit()
+            del obj
+            pbar.update(1)
+            logging.debug("Constructor Results data loaded")
 
-    obj = SprintResultsReader()
-    session.add_all(obj.readFile())
-    session.commit()
-    del obj
+            obj = SprintResultsReader()
+            session.add_all(obj.readFile())
+            session.commit()
+            del obj
+            pbar.update(1)
+            logging.debug("Sprint Results data loaded")
 
-    obj = ResultsReader()
-    session.add_all(obj.readFile())
-    session.commit()
-    del obj
+            obj = ResultsReader()
+            session.add_all(obj.readFile())
+            session.commit()
+            del obj
+            pbar.update(1)
+            logging.debug("Results data loaded")
 
-    obj = QualifyingReader()
-    session.add_all(obj.readFile())
-    session.commit()
-    del obj
+            obj = QualifyingReader()
+            session.add_all(obj.readFile())
+            session.commit()
+            del obj
+            pbar.update(1)
+            logging.debug("Qualifying data loaded")
 
-    obj = PitStopsReader()
-    session.add_all(obj.readFile())
-    session.commit()
-    del obj
+            obj = PitStopsReader()
+            session.add_all(obj.readFile())
+            session.commit()
+            del obj
+            pbar.update(1)
+            logging.debug("Pit Stops data loaded")
 
-    obj = DriverStandingsReader()
-    session.add_all(obj.readFile())
-    session.commit()
-    del obj
+            obj = DriverStandingsReader()
+            session.add_all(obj.readFile())
+            session.commit()
+            del obj
+            pbar.update(1)
+            logging.debug("Driver Standings data loaded")
 
-    obj = LapTimesReader()
-    session.add_all(obj.readFile())
-    session.commit()
-    del obj
+            obj = LapTimesReader()
+            threadPoolExecutor : ThreadPoolExecutor = ThreadPoolExecutor()
+            threadPoolExecutor.submit(session.add_all, obj.readFile())
+            del obj
+            pbar.update(1)
+            logging.debug("Lap Times data loaded")
+    logging.info("All data loaded successfully")
 
 if __name__ == "__main__" :
+    logging.basicConfig(format=f'[%(levelname)s] : [%(asctime)s] : %(message)s | (Line : %(lineno)d) [%(filename)s]',
+                        level=logging.DEBUG,
+                        filename='logs/main_log.log')
+    logging.debug("Logger initialized")
+    logging.debug("Calling read file function")
     read_data_from_files()
+    logging.debug("Read file function execution complete")
+
+    logging.info("Script execution complete")
